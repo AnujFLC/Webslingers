a80c29fd356235ea06e5e703e18209505b633764
raomegh21 <115802868+raomegh21@users.noreply.github.com>
2022-12-18 15:22:43 +0530

    added java script file

---
+++ script.js
@@ -0,0 +1,22 @@
+//Defining UI
+const UIcelsius = document.querySelector('#result-celsius');
+const UIfahrenheit = document.querySelector('#result-fahrenheit');
+const UIkelvin = document.querySelector('#result-kelvin');
+const UIinputValue = document.querySelector('#UIinputValue');
+const UIinputValueUnit = document.querySelector('#UIinputValueUnit');
+
+const jd = document.querySelector('#default');
+const j0 = document.querySelector('#zero');
+const j1 = document.querySelector('#one');
+const j25 = document.querySelector('#twf');
+const j35 = document.querySelector('#thf');
+const j55 = document.querySelector('#fif');
+const j100 = document.querySelector('#hundred');
+
+        thf.style.display = 'none';
+        twf.style.display = 'none';
+        fif.style.display = 'none';
+        zero.style.display = 'none';
+        one.style.display = 'none';
+        hundred.style.display = 'none';
+
